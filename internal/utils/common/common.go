94f2e964c422c8bc02d723fbd967dcf37437961a
JM <hello@jmjanzen.com>
2025-06-13 10:26:36 -0400

    refactor: remove pointless Crash fn
    Can't remember what I was thinking here... Exit code isn't even
    required.

--- internal/utils/common/common.go
+++ internal/utils/common/common.go
@@ -9,10 +9,10 @@ import (
 // Change dir to project root, logging this action
 func ChdirWebserverRoot() {
 	cwd, err := os.Getwd()
-	if err != nil {
-		Crash(err, 1)
+	if err == nil {
+		log.Fatal(err)
 	}
-	log.Println("cwd:" + cwd)
+
 	targetWd := path.Join(cwd, "domains", "jmjanzen.com")
 
 	log.Printf("Changing Working Directory from '%v' to '%v'\n", cwd, targetWd)
@@ -20,8 +20,3 @@ func ChdirWebserverRoot() {
 	os.Chdir(targetWd)
 }
 
-// Log error, and exit using given exit code
-func Crash(err error, exitCode int) {
-	log.Fatal(err)
-	os.Exit(exitCode)
-}
