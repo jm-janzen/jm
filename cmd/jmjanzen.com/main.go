5744f9cfc3036523c3c39fdc36fbe624be7183d7
JM <hello@jmjanzen.com>
2025-06-17 22:46:38 -0400

    refactor: move webserver fs logic to router
    This exposes the generalities between *.jmjanzen.c*

--- cmd/jmjanzen.com/main.go
+++ cmd/jmjanzen.com/main.go
@@ -3,11 +3,8 @@ package main
 import (
 	_ "github.com/joho/godotenv/autoload"
 	"jm/domains/jmjanzen.com/routes"
-	"jm/internal/webserver"
 )
 
 func main() {
-	webserver.Root("domains/jmjanzen.com")
-
 	routes.Launch()
 }
