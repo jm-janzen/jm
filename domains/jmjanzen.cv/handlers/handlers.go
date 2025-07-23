0b1fa0c8d4e37342380b3229ba0ab9f35fd3e629
JM <hello@jmjanzen.com>
2025-07-23 11:22:23 -0400

    feat: wrap resume.html, displaying nav
    This change expects (untracked) résumé files to be present at:
    
        ./domains/jmjanzen.cv/static/{pdf,html,txt}/*

--- domains/jmjanzen.cv/handlers/handlers.go
+++ domains/jmjanzen.cv/handlers/handlers.go
@@ -1,15 +1,30 @@
 package handlers
 
 import (
+	"html/template"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Default(c *gin.Context) {
+	contents, err := os.ReadFile(
+        "./domains/jmjanzen.cv/static/html/resume.html",
+    )
+	if err != nil {
+		log.Println("Error:", err)
+	}
+
+	data := struct {
+		Resume template.HTML
+	}{
+        template.HTML(contents),
+	}
 	c.HTML(
 		http.StatusOK,
-		"resume.html",
-		nil,
+		"home.tmpl",
+		data,
 	)
 }
